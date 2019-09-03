2ed0c18944e5b0c0f7fb03018ce69259170e067d
Luis Ibáñez Alonso <ender3@luiss-imac.home>
2019-09-03 17:21:00 +0200

    added sample

---
+++ sample/hackabot/app/actions.py
@@ -0,0 +1,17 @@
+import time
+from datetime import datetime, date, timedelta
+from config import get_env
+
+
+class Actions:
+	def __init__(self, slackhelper, user_info=None):
+		self.user_info = user_info
+		self.slackhelper = slackhelper
+
+	def help(self):
+		"""
+		Return the Available commands in the system and their usage format
+		"""
+		return {
+			'text': 'Available Commands: \n '
+					'\n `/hackabot help` \n This help information \n \n Hackabot Ver: 1.0'}
